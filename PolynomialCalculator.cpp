982c90883ee244ac9c8226e35252204e6ab2e738
Vladimir Boychev <113020161+VladiHack@users.noreply.github.com>
2024-12-17 11:15:40 +0200

    Finished functionality for the polynomial division. Added a few helper functions and improved the print polynomial function
    Added function for division of Rational Numbers
    Added a nickname for pair<int,int> as rational number for better readability

--- PolynomialCalculator.cpp
+++ PolynomialCalculator.cpp
@@ -8,6 +8,8 @@ using namespace std;
 constexpr int CHOICE_QUIT = 11;
 constexpr int MAX_DEGREE = 1000;
 
+using RationalNumber = pair<int,int>;
+
 
 void addPolynomials();
 void subtractPolynomials();
@@ -19,12 +21,13 @@ void findGCDOfTwoPolynomials();
 void displayVieta();
 void representInPowers();
 void factorAndFindRoots();
-vector<pair<int,int>> readPolynomial();
-void printPolynomial(vector<pair<int,int>> polynomial);
-pair<int, int> readRationalNumber();
+vector<RationalNumber> readPolynomial();
+void printPolynomial(vector<RationalNumber> polynomial);
+RationalNumber readRationalNumber();
 int gcdNums(int a, int b);
 int minNumber(int a, int b);
 int lcmNums(int a, int b);
+vector<RationalNumber> returnMultipliedPolynomialByScalar(vector<RationalNumber> P, RationalNumber scalar);
 
 int main()
 {
@@ -55,7 +58,7 @@ int main()
    
 }
 
-pair<int, int> readRationalNumber() {
+RationalNumber readRationalNumber() {
     int numerator, denominator = 1; // Default denominator is 1
     char slash; // To check for the '/' character
 
@@ -72,14 +75,14 @@ pair<int, int> readRationalNumber() {
     return { numerator, denominator };
 }
 
-vector<pair<int,int>> readPolynomial()
+vector<RationalNumber> readPolynomial()
 {
     int degree;
     cout << "Enter degree of your polynomial>> ";
     cin >> degree;
 
-    vector<pair<int,int>> polynomial;
-    pair<int, int> currentNum;
+    vector<RationalNumber> polynomial;
+    RationalNumber currentNum;
 
     for (int i = degree;i >= 0;i--)
     {
@@ -92,46 +95,84 @@ vector<pair<int,int>> readPolynomial()
     return polynomial;
 }
 
-void printPolynomial(vector<pair<int, int>> polynomial)
+void printPolynomial(vector<RationalNumber> polynomial)
 {
     int size = polynomial.size();
     if (size == 0)
     {
         // HANDLE EXCEPTION
-        cout << "Polynomial is empty";
+        cout << "Polynomial is empty" << endl;
         return;
     }
 
-    pair<int, int> currentNum = polynomial[0];
-    int degree = size -1 ;
-    int numerator, denominator = 1;
+    // Start with the highest degree term
+    int degree = size - 1;
+    bool firstTerm = true;  // To avoid printing "+" at the beginning
 
-    for (int i = 0;i < size;i++)
+    // Traverse each term of the polynomial
+    for (int i = 0; i < size; i++)
     {
-        currentNum = polynomial[i];
+        RationalNumber currentNum = polynomial[i];
+        int numerator = currentNum.first;
+        int denominator = currentNum.second;
 
-        numerator = currentNum.first;
-        denominator = currentNum.second;
-      
-
-        
-
-        if ( numerator!= 0)
+        // Skip zero coefficients
+        if (numerator == 0)
         {
+            degree--;
+            continue;
+        }
 
-            if (i != 0 && numerator > 0 && (numerator != 1 || denominator != 1)) {
-                cout << "+";
+        // If it's not the first term, print "+" for positive coefficients
+        if (!firstTerm && numerator > 0)
+        {
+            cout << "+";
+        }
+
+        // Handle the case when the coefficient is 1 or -1 (don't print the 1 or -1)
+        if (numerator == 1 && denominator == 1)
+        {
+            if (degree == 0)
+            {
+                cout << "1";
             }
-
+            else if(degree==1)
+            {
+                cout << "x";
+            }
+            else {
+                cout << "x^" << degree;
+            }
+        }
+        else if (numerator == -1 && denominator == 1)
+        {
+            if (degree == 0)
+            {
+                cout << "-1";
+            }
+            else if(degree==1)
+            {
+                cout << "-x";
+            }
+            else {
+                cout << "-x^" << degree;
+            }
+        }
+        else
+        {
+            // Print the numerator if it's not 1 or -1
             if (numerator != 1 || denominator != 1)
             {
                 cout << numerator;
             }
 
-            if (denominator != 1) {
+            // Print the denominator if necessary
+            if (denominator != 1)
+            {
                 cout << "/" << denominator;
             }
 
+            // Print the power of x part (skip for degree 0)
             if (degree >= 2)
             {
                 cout << "x^" << degree;
@@ -141,8 +182,11 @@ void printPolynomial(vector<pair<int, int>> polynomial)
                 cout << "x";
             }
         }
-    
-   
+
+        // Mark that it's no longer the first term
+        firstTerm = false;
+
+        // Decrease degree for the next term
         degree--;
     }
 
@@ -200,10 +244,10 @@ int lcmNums(int a, int b)
     return a * b / gcdNums(a, b);
 }
 
-vector<pair<int, int>> addDegree(vector<pair<int, int>> P, int degree)
+vector<RationalNumber> addDegree(vector<RationalNumber> P, int degree)
 {
     //since we have increased the degree,the new P will be of type old P * x^degree + 0x^0 + ... 0x^1 + ... + 0x^degree-1  
-    pair<int, int> zeroCoefficient = { 0,1 };
+    RationalNumber zeroCoefficient = { 0,1 };
     for (int i = 0;i < degree;i++)
     {
         P.push_back(zeroCoefficient);
@@ -212,9 +256,9 @@ vector<pair<int, int>> addDegree(vector<pair<int, int>> P, int degree)
     return P;
 }
 
-pair<int, int> ReturnSumOfElements(pair<int, int> elP, pair<int, int> elQ)
+RationalNumber ReturnSumOfElements(RationalNumber elP, RationalNumber elQ)
 {
-    pair<int, int> elR;
+    RationalNumber elR;
     int numeratorP, denominatorP, numeratorQ, denominatorQ;
     
     numeratorP = elP.first;
@@ -243,10 +287,37 @@ pair<int, int> ReturnSumOfElements(pair<int, int> elP, pair<int, int> elQ)
     return elR;
 }
 
-vector<pair<int, int>> returnSumOfPolynomials(vector<pair<int, int>> P, vector<pair<int, int>> Q)
+RationalNumber ReturnDivisionOfRationalNums(RationalNumber elP, RationalNumber elQ)
+{
+    RationalNumber result;
+
+    // Example : 2/3 / ( 4 / 5) = 2/3 * 5/4 = 10/12  = 5/6
+
+    int numeratorP, denominatorP, numeratorQ, denominatorQ, numeratorResult, denominatorResult;
+
+    numeratorP = elP.first;
+    denominatorP = elP.second;
+    numeratorQ = elQ.first;
+    denominatorQ = elQ.second;
+
+    //we flip the second rational
+    numeratorResult = numeratorP * denominatorQ; 
+    denominatorResult = numeratorQ * denominatorP;
+     
+    //now we make sure that it is simplified
+    int gcd = gcdNums(numeratorResult, denominatorResult);
+    numeratorResult /= gcd;
+    denominatorResult /= gcd;
+
+    result = { numeratorResult, denominatorResult };
+   
+    return result;
+}
+
+vector<RationalNumber> returnSumOfPolynomials(vector<RationalNumber> P, vector<RationalNumber> Q)
 {
     //resulting vector
-    vector<pair<int, int>> R;
+    vector<RationalNumber> R;
 
     int sizeP = P.size();
     int sizeQ = Q.size();
@@ -273,7 +344,7 @@ vector<pair<int, int>> returnSumOfPolynomials(vector<pair<int, int>> P, vector<p
         }
     }
 
-    pair<int, int> elementR;
+    RationalNumber elementR;
     while (indexP != sizeP && indexQ != sizeQ)
     {
         elementR = ReturnSumOfElements(P[indexP], Q[indexQ]);
@@ -286,7 +357,20 @@ vector<pair<int, int>> returnSumOfPolynomials(vector<pair<int, int>> P, vector<p
     return R;
 }
 
-vector<pair<int, int>> returnMultipliedPolynomialByScalar(vector<pair<int, int>> P, pair<int, int> scalar)
+vector<RationalNumber> returnSubtractionOfPolynomials(vector<RationalNumber> P, vector<RationalNumber> Q)
+{
+    //We will use the following rule : a + b = a + (-b) and since we already have the function for sum of two polynomials we just need to multiply the second polynomial by -1
+   //So we will first write the function for multiplying polynomial by scalar
+    RationalNumber coefficient = { -1,1 }; // this is exactly the number -1 
+    Q = returnMultipliedPolynomialByScalar(Q, coefficient);
+
+    //R will be the result of the subtraction
+    vector<RationalNumber> R = returnSumOfPolynomials(P, Q);
+
+    return R;
+}
+
+vector<RationalNumber> returnMultipliedPolynomialByScalar(vector<RationalNumber> P, RationalNumber scalar)
 {
     int size = P.size();
     int gcd = 0; 
@@ -317,17 +401,17 @@ vector<pair<int, int>> returnMultipliedPolynomialByScalar(vector<pair<int, int>>
     return P;
 }
 
-vector<pair<int, int>> returnMultiplicationOfPolynomials(vector<pair<int, int>> P, vector<pair<int, int>> Q)
+vector<RationalNumber> returnMultiplicationOfPolynomials(vector<RationalNumber> P, vector<RationalNumber> Q)
 {
     //Multiplication of two polynomials P(x) and Q(x).
     //Let's assume P(x) is 5x^3 + 3x^2 + 1 and Q(x) is x^2 + 3
     // Then P(x) * Q(x) = P(x) * x^2 + P(x) * 3 which is sum of polynomials multiplied by scalar and and x of some degree.
 
     //Resulting polynomial
-    vector<pair<int, int>> R;
+    vector<RationalNumber> R;
     
     int sizeQ = Q.size();
-    vector<pair<int, int>> multipliedPolynomial;
+    vector<RationalNumber> multipliedPolynomial;
     int degree = sizeQ - 1;
 
     for (int i = 0;i < sizeQ;i++)
@@ -353,19 +437,70 @@ vector<pair<int, int>> returnMultiplicationOfPolynomials(vector<pair<int, int>> 
     return R;
 }
 
+vector<RationalNumber> returnDivisionOfPolynomials(vector<RationalNumber> & P, vector<RationalNumber> Q)
+{
+    // the division of two polynomials can be solved using subtraction of two polynomials and multiplication of polynomial by a number
+    // example : P(x) = 5x^5 - 4x^4 + 3x^3 + x^2 + x + 1
+    // Q(x) = x^3 - 5
+    //we need to find a coefficient, which will remove the element of highest degree in P(x). We do this until the highest degree in P(x) < highest degree in Q(x)
+    
+    vector<RationalNumber> Quotient;
+
+
+    int degreeDifference = P.size() - Q.size();
+
+    int indexP = 0; 
+    int indexQ = 0;
+
+
+    RationalNumber coefficient;
+    RationalNumber zero = { 0,1 };
+    vector<RationalNumber> tempPolynomial;
+    
+
+
+    while (degreeDifference>=0)
+    {
+        if (P[indexP].first == 0)
+        {
+            indexP++; 
+            degreeDifference--;
+            Quotient.push_back(zero);
+            continue;
+        }
+        //We find the coefficient that Q needs to by multiplied by in order to remove the highest degree of P(x) 
+        coefficient = ReturnDivisionOfRationalNums(P[indexP], Q[indexQ]);
+        //Now we multiply Q by coefficient and subtract it from P
+        tempPolynomial = returnMultipliedPolynomialByScalar(Q, coefficient);
+        //Now we increase the degree of the polynomial
+        tempPolynomial = addDegree(tempPolynomial, degreeDifference);
+        //now we subtract P and the tempPolynomial
+        P = returnSubtractionOfPolynomials(P, tempPolynomial);
+        //We add the coefficient to the Quotient polynomial
+        Quotient.push_back(coefficient);
+        degreeDifference--; indexP++;
+    }
+
+    //At the end, our P(x) will have become the remainder
+
+
+    return Quotient;
+
+}
+
 void addPolynomials() {
     cout << "Enter polynomial P(x)" << endl;
-    vector<pair<int,int>> P = readPolynomial();
+    vector<RationalNumber> P = readPolynomial();
     cout << "P(X) = ";
     printPolynomial(P);
 
     cout << "Enter polynomial Q(x)" << endl;
-    vector<pair<int,int>> Q = readPolynomial();
+    vector<RationalNumber> Q = readPolynomial();
     cout << "Q(X) = ";
     printPolynomial(Q);
 
 
-    vector<pair<int, int>> R = returnSumOfPolynomials(P, Q);
+    vector<RationalNumber> R = returnSumOfPolynomials(P, Q);
 
     
     cout << "P(X) + Q(X) = ";
@@ -374,23 +509,17 @@ void addPolynomials() {
 
 void subtractPolynomials() {
     cout << "Enter polynomial P(x)" << endl;
-    vector<pair<int, int>> P = readPolynomial();
+    vector<RationalNumber> P = readPolynomial();
     cout << "P(X) = ";
     printPolynomial(P);
 
     cout << "Enter polynomial Q(x)" << endl;
-    vector<pair<int, int>> Q = readPolynomial();
+    vector<RationalNumber> Q = readPolynomial();
     cout << "Q(X) = ";
     printPolynomial(Q);
 
 
-    //We will use the following rule : a + b = a + (-b) and since we already have the function for sum of two polynomials we just need to multiply the second polynomial by -1
-    //So we will first write the function for multiplying polynomial by scalar
-    pair<int, int> coefficient = { -1,1 }; // this is exactly the number -1 
-    Q = returnMultipliedPolynomialByScalar(Q, coefficient);
-
-    //R will be the result of the subtraction
-    vector<pair<int, int>> R = returnSumOfPolynomials(P, Q);
+    vector<RationalNumber> R = returnSubtractionOfPolynomials(P, Q);
     cout << "P(x) - Q(x) = ";
     printPolynomial(R);
    
@@ -401,17 +530,17 @@ void multiplyPolynomials() {
  
    
     cout << "Enter polynomial P(x)" << endl;
-    vector<pair<int, int>> P = readPolynomial();
+    vector<RationalNumber> P = readPolynomial();
     cout << "P(X) = ";
     printPolynomial(P);
 
     cout << "Enter polynomial Q(x)" << endl;
-    vector<pair<int, int>> Q = readPolynomial();
+    vector<RationalNumber> Q = readPolynomial();
     cout << "Q(X) = ";
     printPolynomial(Q);
 
 
-    vector<pair<int, int>> R = returnMultiplicationOfPolynomials(P, Q);
+    vector<RationalNumber> R = returnMultiplicationOfPolynomials(P, Q);
 
 
     cout << "P(X) * Q(X) = ";
@@ -419,18 +548,34 @@ void multiplyPolynomials() {
 }
 
 void dividePolynomials() {
-    cout << "dividePolynomials() is not implemented yet.\n";
+    cout << "Enter polynomial P(x)" << endl;
+    vector<RationalNumber> P = readPolynomial();
+    cout << "P(X) = ";
+    printPolynomial(P);
+
+    cout << "Enter polynomial Q(x)" << endl;
+    vector<RationalNumber> Q = readPolynomial();
+    cout << "Q(X) = ";
+    printPolynomial(Q);
+
+
+    //R will be the result of the division
+    vector<RationalNumber> Quotient = returnDivisionOfPolynomials(P, Q);
+    cout << "Quotient Q(x)=";
+    printPolynomial(Quotient);
+    cout << "Remainder R(x)=";
+    printPolynomial(P);
 }
 
 void multiplyPolynomialByScalar() {
 
     cout << "Enter polynomial P(x)" << endl;
-    vector<pair<int, int>> P = readPolynomial();
+    vector<RationalNumber> P = readPolynomial();
     cout << "P(X) = ";
     printPolynomial(P);
 
     cout << "Enter rational number>> ";
-    pair<int, int> scalar = readRationalNumber();
+    RationalNumber scalar = readRationalNumber();
 
     P = returnMultipliedPolynomialByScalar(P, scalar);
     cout << "Result: ";
